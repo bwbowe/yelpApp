b4b96b58772de989eed65e31be7a925222ca6756
bwbowe <59293142+bwbowe@users.noreply.github.com>
2020-04-01 20:20:02 -0500

    first commit

---
+++ src/components/App/App.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import BusinessList from '../BusinessList/BusinessList.js';
+import SearchBar from '../SearchBar/SearchBar.js';
+import './App.css'
+import Yelp from '../../util/Yelp';
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      businesses: []
+    };
+    this.searchYelp = this.searchYelp.bind(this);
+  };
+
+  searchYelp(term, location, sortBy) {
+    Yelp.search(term, location, sortBy).then(businesses => {
+      this.setState({
+        businesses: businesses
+      });
+    });
+  };
+  
+  render() {
+    return (
+      <div className="App">
+        <h1>Yarp</h1>
+        <SearchBar searchYelp={this.searchYelp} />
+        <BusinessList businesses={this.state.businesses} /> 
+      </div>
+    );
+  }
+}
+
+export default App;
